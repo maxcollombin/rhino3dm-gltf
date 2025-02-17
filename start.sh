10a70ad9defc4baa52f6694b1edd051fac1d3930
maxcollombin <maxime.collombin@admin.vs.ch>
2025-02-17 16:21:21 +0100

    refactor: rename R21 to rhino3dm2gltf and update related scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 -m venv R21
-. R21/bin/activate
+python3 -m venv rhino3dm2gltf
+. rhino3dm2gltf/bin/activate
 pip install rhino3dm
--- stop.sh
+++ stop.sh
@@ -3,4 +3,4 @@
 if command -v deactivate >/dev/null 2>&1; then
 	deactivate
 fi
-rm -rf R21
+rm -rf rhino3dm2gltf
